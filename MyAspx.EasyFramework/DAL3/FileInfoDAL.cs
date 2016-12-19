fd50c4d6721403d94038e30409f29dd229c8c02e
manpaiboy <635434924@qq.com>
2016-12-19 18:59:03 +0800

    增加添加源码基础信息入库

---
+++ MyAspx.EasyFramework/DAL3/FileInfoDAL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyAspx.EasyFramework.EntityFramework;
+
+namespace MyAspx.EasyFramework.DAL3
+{
+    /// <summary>
+    /// 新源码的基础信息DAL
+    /// </summary>
+    public class FileInfoDAL
+    {
+        commyaspxdbEntities _myaspxentities = new commyaspxdbEntities();
+
+        /// <summary>
+        /// 增加一条新上传的源代码的基础信息
+        /// </summary>
+        /// <param name="_my_fileinfo"></param>
+        /// <returns></returns>
+        public int AddNewCodeBaseInfo(my_fileinfo _my_fileinfo)
+        {
+            var fFileName = _my_fileinfo.filename;
+            if (_myaspxentities.my_fileinfo.Count(SELECT => SELECT.filename == fFileName) > 0)
+            {
+                //同名的不插入
+                return -1;
+            }
+            _myaspxentities.my_fileinfo.Add(new MyAspx.EasyFramework.EntityFramework.my_fileinfo()
+            {
+                filename =  _my_fileinfo.filename,
+                filedatabase = _my_fileinfo.filedatabase,
+                filedevlan = _my_fileinfo.filedevlan,
+                filevstype = _my_fileinfo.devtool,
+                filepics = _my_fileinfo.filepics,
+                codedes = _my_fileinfo.codedes,
+                codelang =  _my_fileinfo.codelang,
+                contract = _my_fileinfo.contract,
+                createuser =  _my_fileinfo.createuser,
+                codeplftype = _my_fileinfo.frameworkversion,
+            });
+            _myaspxentities.SaveChanges();
+            return 1;
+        }
+        public my_fileinfo HasEqualsCodeName(string fName)
+        {
+            var _myfileinfoobject = from _myfo in _myaspxentities.my_fileinfo where _myfo.filename.Equals(fName) select _myfo;
+            if (_myfileinfoobject.Count() > 0)
+                return new my_fileinfo { filename = fName };
+            else
+                return null;
+        }
+    }
+}
--- WebUI/Services/Core/SendCodeInfoHandler.ashx.cs
+++ WebUI/Services/Core/SendCodeInfoHandler.ashx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
+using MyAspx.EasyFramework.DAL3;
+using MyAspx.EasyFramework.EntityFramework;
 
 namespace WebUI.Services.Core
 {
@@ -10,12 +13,61 @@ namespace WebUI.Services.Core
     /// </summary>
     public class SendCodeInfoHandler : IHttpHandler, System.Web.SessionState.IRequiresSessionState
     {
-
+        FileInfoDAL _fileInfoDal = new FileInfoDAL();
         public void ProcessRequest(HttpContext context)
         {
             context.Response.ContentType = "text/plain";
             //主要处理逻辑
-            
+            var result = new []
+            {
+                //源码名称 0
+                context.Request.Form["name"],            //1
+                //源码的价格 1
+                context.Request.Form["paynub"],       
+                //源码下载的服务端地址 2
+                context.Request.Form["codeserverurl"],    
+                //源码图片集合 3
+                context.Request.Form["codepics"],        //1
+                //源码的类别 4
+                context.Request.Form["codetype"],        
+                //源码开发工具 5
+                context.Request.Form["developtool"],      //1
+                //源码的编程语言 6
+                context.Request.Form["language"],        //1
+                //源码使用的数据库 7
+                context.Request.Form["database"],        //1
+                //源码的.NET 框架 8
+                context.Request.Form["framework"],        //1
+                //源码标签 9
+                context.Request.Form["tags"],            
+                //源码外连接地址 10
+                context.Request.Form["linkurl"],          
+                //源码作者 11
+                context.Request.Form["author"],           //1
+                //源码作者联系方式 12
+                context.Request.Form["contract"],        //1
+                //源码的作者留言 13
+                context.Request.Form["usermsg"],        
+                //源码详细描述 14
+                context.Request.Form["preview"]          //1
+
+            };
+            //源码基础详细入库
+            int exec = _fileInfoDal.AddNewCodeBaseInfo(new my_fileinfo()
+            {
+                filename = result[0],
+                filedatabase = result[7],
+                filedevlan = result[6],
+                filevstype = result[5],
+                filepics = result[3],
+                codedes = result[14],
+                codelang = result[6],
+                contract = result[12],
+                createuser = result[11],
+                codeplftype = result[5],
+                devtool = result[5],
+                frameworkversion = result[8],
+              }); 
             //结束
             context.Response.End();
         }
